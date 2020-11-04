439a05d912ee26089a7dc6d756bfe9d292839101
DarkKnight <piyushmusaddi12@gmail.com>
2020-11-04 15:55:17 +0530

    updeated

--- test2.cpp
+++ test2.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main()
 {
     for(int i=0;i<10;i++)
-        cout<<"hello";
+        cout<<"hello\n";
+    cout<<"done with for loop\n";
+    cout<<"what about while loops\n";
 }
